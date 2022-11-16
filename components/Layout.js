da9941754775e20bfd9ffb9fe7f74b74ee67c347
VITALIY1986 <staslego1986@gmail.com>
2022-11-16 10:30:54 +0200

    asd

--- components/Layout.js
+++ components/Layout.js
@@ -10,14 +10,15 @@ const Layout = (props) => {
     <title>{props.title}</title>
  
   </Head>
-  <Script strategy="lazyOnload">
-    {`
-   window.replainSettings = { id: '315457bd-ba97-4b76-bde2-83c365bb4708' };
-  (function(u){var s=document.createElement('script');s.async=true;s.src=u;
-  var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
-  })('https://widget.replain.cc/dist/client.js');
-    `}
-  </Script>
+
+				<Script strategy="lazyOnload">
+        {`
+            window.replainSettings = { id: '57c23d8c-ad58-4f57-b0bc-d65f546cf0d4' };
+            (function(u){var s=document.createElement('script');s.async=true;s.src=u;
+            var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+            })('https://widget.replain.cc/dist/client.js');
+        `}
+      </Script>
         <div className="wrapper">
         
        
