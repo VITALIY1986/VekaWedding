1dc21d467608ff7059c5cbdb2161431fe6663ee9
VITALIY1986 <staslego1986@gmail.com>
2022-11-16 15:51:00 +0200

    sdf

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.scss'
 import Link from 'next/link';
 import Logo from "../public/Logo.png"
 import Favicon from "../public/favicon.png"
-
+import About from "../public/about-as.jpg"
 import Layout from '../components/Layout.js'
 export default function Home() {
   
@@ -30,12 +30,23 @@ export default function Home() {
         <div className='dot'></div>
 		</label>
 		<section className="drawer-list bg-very_peri  ">
-		<ul className="text-center mt-30 uppercase">
-		<li className='mx-5 mt-10'><a href="/">Acasa</a></li>
-			<li className='mx-5 mt-10'><a href="/categories">produse</a></li>
-			<li className='mx-5 mt-10'><a href="/contact">Contact</a></li>
-			
-		</ul>
+		<div className="abot-us ">
+      <div className='grid-about-us'>
+    
+    <Image
+        src={About}
+        alt="Picture of the author"
+        width={250}
+      />
+   
+   </div>
+   <div className='grid-about-us'>
+      <h1><span >A</span>BOUT US</h1>
+      <p>VekaWedding has long become an exemplary wedding dresses manufacturer, the name of whichsounds familiar to everyone in the industry. All this is thanks to our founders and professional tailors, with long-term experience in the wedding business.</p>
+   
+   
+   </div>
+		</div>
 			</section>
 
       </div>
@@ -66,7 +77,7 @@ export default function Home() {
 		alt="Picture of the author"
 		width={42}
 	  /><p>vekawedding.com</p></button>
-      <button className={styles.c_button }> <div className={styles.marquees } ><span className={styles.marquee }>&#128150; Modele de modă de rochii de mireasă în 2023. 	&#128141; </span></div></button>
+      <button className={styles.c_button }> <div className={styles.marquees } ><span className={styles.marquee }>	&#128141; Wedding dress fashion models in 2023. 	&#128141; </span></div></button>
       <button className={styles.c_button } ><p>Informatii pentru parteneri</p></button>
       <button className={styles.c_button }><p>Blog</p></button>
      
