e73e0f13af1e9c176f98a843167ffb03781cfd7a
VITALIY1986 <staslego1986@gmail.com>
2022-11-16 02:01:30 +0200

    sdf

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,17 @@ import Favicon from "../public/favicon.png"
 import Script from 'next/script'
 
 export default function Home() {
+  
   return (
-
-   
+<>
+    <Script strategy="lazyOnload">
+    {`
+   window.replainSettings = { id: '315457bd-ba97-4b76-bde2-83c365bb4708' };
+  (function(u){var s=document.createElement('script');s.async=true;s.src=u;
+  var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+  })('https://widget.replain.cc/dist/client.js');
+    `}
+  </Script>
     <div className={styles.container}>
   
       <video autoPlay muted loop className={styles.video_background }
@@ -66,16 +74,9 @@ export default function Home() {
           
           {/*  <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />*/}
       
-          <Script strategy="lazyOnload">
-    {`
-   window.replainSettings = { id: '315457bd-ba97-4b76-bde2-83c365bb4708' };
-(function(u){var s=document.createElement('script');s.async=true;s.src=u;
-var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
-})('https://widget.replain.cc/dist/client.js');
-    `}
-  </Script>
+         
     
     </div>
-
+    </>
   )
 }
